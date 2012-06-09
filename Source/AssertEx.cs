01a1104f8f744fac96284375ec2c95e839722a2d
Brendan Forster <me@brendanforster.com>
2012-06-09 09:58:36 +0200

    indenting

--- Source/AssertEx.cs
+++ Source/AssertEx.cs
@@ -1,21 +1,20 @@
 using System;
-using AssertExLib;
 using AssertExLib.Internal;
 using AssertExLib.Exceptions;
 
 namespace AssertExLib
 {
-	public delegate void ThrowsDelegate();
-	public delegate object ThrowsDelegateWithReturn();
+    public delegate void ThrowsDelegate();
+    public delegate object ThrowsDelegateWithReturn();
 
-	public static class AssertEx
-	{
-		public static void DoesNotThrow(ThrowsDelegate testCode)
-		{
-			var ex = Recorder.Exception(testCode);
-			if (ex != null)
-				throw new AssertExException("DoesNotThrow failed.", ex);
-		}
+    public static class AssertEx
+    {
+        public static void DoesNotThrow(ThrowsDelegate testCode)
+        {
+            var ex = Recorder.Exception(testCode);
+            if (ex != null)
+                throw new AssertExException("DoesNotThrow failed.", ex);
+        }
 
         public static void DoesNotThrow(ThrowsDelegateWithReturn testCode)
         {
@@ -24,41 +23,41 @@ namespace AssertExLib
                 throw new AssertExException("DoesNotThrow failed.", ex);
         }
 
-		public static T Throws<T>(ThrowsDelegate testCode) where T : Exception
-		{
-			return (T) Throws(typeof (T), testCode);
-		}
+        public static T Throws<T>(ThrowsDelegate testCode) where T : Exception
+        {
+            return (T)Throws(typeof(T), testCode);
+        }
 
-		public static T Throws<T>(ThrowsDelegateWithReturn testCode) where T : Exception
-		{
-			return (T) Throws(typeof (T), testCode);
-		}
+        public static T Throws<T>(ThrowsDelegateWithReturn testCode) where T : Exception
+        {
+            return (T)Throws(typeof(T), testCode);
+        }
 
-		public static Exception Throws(Type exceptionType, ThrowsDelegate testCode)
-		{
-			var exception = Recorder.Exception(testCode);
+        public static Exception Throws(Type exceptionType, ThrowsDelegate testCode)
+        {
+            var exception = Recorder.Exception(testCode);
 
-			if (exception == null)
-				throw new AssertExException("AssertExtensions.Throws failed. No exception occurred.");
+            if (exception == null)
+                throw new AssertExException("AssertExtensions.Throws failed. No exception occurred.");
 
-			if (!exceptionType.Equals(exception.GetType()))
-				throw new AssertExException(String.Format("AssertExtensions.Throws failed. Incorrect exception {0} occurred.", exception.GetType().Name), exception);
+            if (!exceptionType.Equals(exception.GetType()))
+                throw new AssertExException(String.Format("AssertExtensions.Throws failed. Incorrect exception {0} occurred.", exception.GetType().Name), exception);
 
-			return exception;
-		}
+            return exception;
+        }
 
-		public static Exception Throws(Type exceptionType, ThrowsDelegateWithReturn testCode)
-		{
-			var exception = Recorder.Exception(testCode);
+        public static Exception Throws(Type exceptionType, ThrowsDelegateWithReturn testCode)
+        {
+            var exception = Recorder.Exception(testCode);
 
-			if (exception == null)
-				throw new AssertExException("AssertExtensions.Throws failed. No exception occurred.");
+            if (exception == null)
+                throw new AssertExException("AssertExtensions.Throws failed. No exception occurred.");
 
-			if (!exceptionType.Equals(exception.GetType()))
-				throw new AssertExException(String.Format("AssertExtensions.Throws failed. Incorrect exception {0} occurred.", exception.GetType().Name), exception);
+            if (!exceptionType.Equals(exception.GetType()))
+                throw new AssertExException(String.Format("AssertExtensions.Throws failed. Incorrect exception {0} occurred.", exception.GetType().Name), exception);
 
-			return exception;
-		}
-	}
+            return exception;
+        }
+    }
 }
 
